ac831939a367b69fa1acf94b5eedef075aad651d
hyun <acd2283@gmail.com>
2024-10-06 19:24:12 +0900

    hashtag-finish

--- main/java/com/example/demo0810/repository/PostTagMapRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo0810.repository;
-
-import com.example.demo0810.Entity.PostTagMap;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostTagMapRepository extends JpaRepository<PostTagMap, Long> {
-}
-
